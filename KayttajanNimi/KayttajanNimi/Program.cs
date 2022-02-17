ae12a4b6f7dc8fd0b1f05bf8d7605b9fa3e6cf77
katrimari <katri.lepola@edu.keuda.fi>
2022-02-17 15:21:44 +0200

    lisäys

---
+++ KayttajanNimi/KayttajanNimi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KayttajanNimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Kerro etu nimesi");
+            string nimi = Console.ReadLine();
+            Console.Write("Hei " + nimi);
+            Console.Read();
+        }
+    }
+}
---
+++ YksiNumerot/YksiNumerot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YksiNumerot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku = 1;
+            int ruku = 11;
+            int muku = 101;
+            int nuku = 1001;
+            Console.WriteLine(luku);
+            Console.WriteLine(ruku);
+            Console.WriteLine(muku);
+            Console.WriteLine(nuku);
+            Console.Read();
+        }
+    }
+}
